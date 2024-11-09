e73dc99ba4d868514b563f496bbe9c99a9abc603
tDepOwO <tiep2403nbk@gmail.com>
2024-11-08 22:08:45 -0500

    Update page.tsx

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -5,7 +5,7 @@ import GraphView from "../components/Graph";
 export default function Page(){
     return (
         <div>
-            <GraphView></GraphView>
+            <GraphView></GraphVie
         </div>
     )
 }
