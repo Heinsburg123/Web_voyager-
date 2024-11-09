534a1029fb49ff6e0cb2b7c84c016333344d3763
hoangquan456 <truonghoangquan456@gmail.com>
2024-11-09 00:57:23 -0500

    update useState for Graph

--- src/app/components/Graph.js
+++ src/app/components/Graph.js
@@ -1,41 +1,15 @@
 "use client"
 
-import React from 'react';
+import React, { useState, useEffect,  } from 'react';
 import dynamic from 'next/dynamic';
 import { fetchGraphData } from '../lib/actions';
 
 const Graph = dynamic(() => import('react-graph-vis'), { ssr: false });
 
-export default function GraphView(graphData) {
-    var graph = {
-        nodes: [
-            { id: 1, label: "node 1", level: 0 },  // Root node on the left
-            { id: 2, label: "node 2", level: 1 },
-            { id: 3, label: "node 3", level: 2 },
-            { id: 4, label: "node 4", level: 1 },
-            { id: 5, label: "node 5", level: 1 },
-            { id: 6, label: "node 6", level: 3 },
-            { id: 7, label: "node 7", level: 4 },
-            { id: 8, label: "node 8", level: 5 },
-            { id: 9, label: "node 9", level: 6 },
-            { id: 10, label: "node 10", level: 7 },
-            { id: 11, label: "node 11", level: 8 },
-        ],
-        edges: [
-            { from: 1, to: 2 },
-            { from: 2, to: 3 },
-            { from: 1, to: 4 },
-            { from: 1, to: 5 },
-            { from: 3, to: 6 },
-            { from: 6, to: 7 },
-            { from: 7, to: 8 },
-            { from: 8, to: 9 },
-            { from: 9, to: 10 },
-            { from: 10, to: 11 },
-        ],
-    };
+export default function GraphView( {graphData} ) {
+    const [selectedNodeId, setSelectedNodeId] = useState(0);
 
-    var data = graphData.graphData;
+    var data = graphData;
     var rAdj = [], Level = [];
 
     function dfs(u) {
@@ -76,10 +50,7 @@ export default function GraphView(graphData) {
         
         return graph;        
     }
-    //const data2 = createGraph();
-    //console.log( data2 ); 
-    graph = createGraph();
-    console.log(Level);
+    
 
     const options = {
         layout: {
@@ -112,10 +83,13 @@ export default function GraphView(graphData) {
 
     const events = {
         select: ({ nodes }) => {
-            //lam trong day
-            
+            // console.log(graph);
+            setSelectedNodeId(0);
+            // console.log(graph);
         },
       };
+
+    const [graph, setGraph] = useState(createGraph());
 
     return (
         <div className = "h-screen">
